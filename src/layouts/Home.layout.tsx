ca6dc3d726dd05e0d6ea1c3a166617c488ee33f5
Rahul <rahu.adsps@outlook.com>
2025-03-19 17:05:53 +0530

    removing dist folder

--- src/layouts/Home.layout.tsx
+++ src/layouts/Home.layout.tsx
@@ -5,7 +5,6 @@ import { ReactNode } from 'react';
 interface HomeLayoutProps {
     children: ReactNode;
 }
-
 export default function HomeLayout({ children }: HomeLayoutProps) {
     return (
         <>
